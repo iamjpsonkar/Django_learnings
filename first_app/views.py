e11e41266672a42f58ea13acf811b14291dd897b
Jay Prakash Sonkar <iamjpsonkar@gmail.com>
2022-02-05 22:14:58 +0530

    Using Templates
    Create first_project/templates/first_app/index.html
    - add your code in new index.html
    
    - open first_project/first_project/settings.py
    - add below lines ,after BASE_DIR defination
    -- import os
    -- TEMPLATES_DIR = os.path.join(BASE_DIR,"templates")
    - find TEMPLATE variable and add below value in DIR
    -- TEMPLATES_DIR,
    
    Open first_app/views.py
    - add below lines
    -- from django.shortcuts import render
    - change defination of index function
    -- def index(request=None):
    --     my_dict={'myname':'Jay Prakash Sonkar'}
    --     return render(request,'first_app/index.html',context=my_dict)
    
    Save everything and rerun server to see your output

--- first_app/views.py
+++ first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request=None):
-    return HttpResponse("<h1>Hello World!!!</h1>")
+    my_dict={'myname':'Jay Prakash Sonkar'}
+    return render(request,'first_app/index.html',context=my_dict)
